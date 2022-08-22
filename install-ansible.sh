76462d2957fd3f562a6534f2fb558f7465ee05dd
G. L. Clark, II <gary.clark@shu.edu>
2022-08-22 14:00:18 -0400

    - Moves Python task - Comments tasks in setup-fedora-... that have been moved to specific tasks - Adds Python and debugging to Ansible install script.

--- install-ansible.sh
+++ install-ansible.sh
@@ -5,11 +5,17 @@ echo "Updating the system..."
 sudo dnf update -y
 
 # Install ansible
-echo "Installing ansible and dialog..."
-sudo dnf install -y ansible dialog
+echo "Installing ansible, dialog, and python..."
+sudo dnf install -y ansible dialog python
 
-# Check the version of Ansible
+# Check the version of installed packages
 echo "Installed version of Ansible:"
 ansible --version
+
+echo "Installed version of Dialog:"
+dialog --version
+
+echo "Installed version of Python:"
+python --version
 
 # eof
