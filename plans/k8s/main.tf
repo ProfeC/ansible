50d2b599c6e15df61a9fa0549cd505c51cda75d2
ProfeC <gclark2@gmail.com>
2024-02-18 20:02:46 -0500

    bunch-o-changes!

--- plans/k8s/main.tf
+++ plans/k8s/main.tf
@@ -9,53 +9,54 @@ terraform {
 
 provider "proxmox" {
   # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
-  pm_api_url = "https://192.168.1.11:8006/api2/json"
+  pm_api_url = var.proxmox_host["api_url"]
 
   # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = "root@pam!terraform"
+  pm_api_token_id = var.proxmox_host["api_token_id"]
 
   # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  pm_api_token_secret = "27f5f065-97cf-458b-817e-19451852e394"
+  pm_api_token_secret = var.proxmox_host["api_token_secret"]
 
   # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
 }
 
-# resource is formatted to be "[type]" "[entity_name]" so in this case
-# we are looking to create a proxmox_vm_qemu entity named test_server
+# resource is formatted to be "[type]" "[entity_name]" so in this case we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "test_server" {
-  count = 1 # just want 1 for now, set to 0 and apply to destroy VM
+  # another variable with contents 
+  clone = var.template_name
+  full_clone = true
+  
+  for_each = var.servers
 
-  name = "test-vm-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+  name = "test-kube-${each.key}"
 
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
-  target_node = var.proxmox_host
-
-  # another variable with contents "ubuntu-2004-cloudinit-template"
-  clone = var.template_name
+  target_node = var.proxmox_host["node"]
 
   # basic VM settings here. agent refers to guest agent
-  agent = 0
-  bootdisk = "scsi0"
-  ciuser = "serveradmin"
-  cloudinit_cdrom_storage = "fast-storage"
+  agent = 1
   cores = 2
   cpu = "host"
   desc = "This was generated with terraform"
+  # force_create = true # force recreation during testing.
   memory = 2048
   numa = true
+  onboot = true
   os_type = "cloud-init"
-  # scsihw = "virtio-scsi-pci"
-  scsihw   = "virtio-scsi-single" 
   sockets = 2
   vcpus = 0
-  vmid = 800
+  vmid = each.value["vmid"]
+
+  bootdisk = "scsi0"
+  cloudinit_cdrom_storage = "fast-storage"
+  scsihw   = "virtio-scsi-single" 
 
   disks {
     scsi {
       scsi0 {
         disk {
-          size = 10
+          size = 25
           storage = "datahog"
           # iothread = true
         }
@@ -68,8 +69,7 @@ resource "proxmox_vm_qemu" "test_server" {
     model = "virtio"
     bridge = "vmbr0"
     tag = 20
-    # macaddr = "B3:F5:02:23:6F:3F"
-    macaddr = "BC:24:11:0B:25:F2"
+    macaddr = each.value["mac_addr"]
   }
 
   # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
@@ -84,10 +84,71 @@ resource "proxmox_vm_qemu" "test_server" {
   # be 10.98.1.91 since count.index starts at 0. this is how you can create
   # multiple VMs and have an IP assigned to each (.91, .92, .93, etc.)
 
-  # ipconfig0 = "ip=10.98.1.9${count.index + 1}/24,gw=10.98.1.1"
+  # ipconfig0 = "ip=192.168.20.2${count.index + 1}/24,gw=192.168.20.1"
+  ipconfig0 = ""
+
+  # Cloudinit user
+  ciuser = "serveradmin"
   
-  # sshkeys set using variables. the variable contains the text of the key.
-  sshkeys = <<EOF
-  ${var.ssh_key}
-  EOF
+  # # sshkeys set using variables. the variable contains the text of the key.
+  # sshkeys = <<EOF
+  # ${var.ssh_key}
+  # EOF
+}
+/*
+resource "proxmox_vm_qemu" "test_server" {
+  # another variable with contents 
+  clone = var.template_name
+  full_clone = true
+  
+  name = "test-vm-${count.index + 1}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+
+  target_node = var.proxmox_host["node"]
+
+  agent = 1
+  cores = 2
+  cpu = "host"
+  desc = "This was generated with terraform"
+  memory = 2048
+  numa = true
+  onboot = true
+  os_type = "cloud-init"
+  sockets = 2
+  vcpus = 0
+  vmid = 800
+
+  bootdisk = "scsi0"
+  cloudinit_cdrom_storage = "fast-storage"
+  scsihw   = "virtio-scsi-single" 
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          size = 25
+          storage = "datahog"
+          # iothread = true
+        }
+      }
+    }
+  }
+  
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+    tag = 20
+    macaddr = "92:3f:d1:16:07:c2"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network, tag,
+    ]
+  }
+
+  # ipconfig0 = "ip=192.168.20.2${count.index + 1}/24,gw=192.168.20.1"
+  ipconfig0 = ""
+
+  # Cloudinit user
+  ciuser = "serveradmin"
 }
--- plans/k8s/vars.tf
+++ plans/k8s/vars.tf
@@ -3,9 +3,90 @@ variable "ssh_key" {
 }
 
 variable "proxmox_host" {
-  default = "morpheus"
+  type = map
+  default = {
+    node = "morpheus"
+    api_url = "https://192.168.1.11:8006/api2/json"
+    api_token_id = "root@pam!terraform"
+    api_token_secret = "27f5f065-97cf-458b-817e-19451852e394"
+  }
 }
 
 variable "template_name" {
   default = "debian12-cloud"
+}
+
+variable "gateway" {
+  type = string
+  default = "192.168.20.1"
+}
+
+variable "servers" {
+  type = map(object({
+    ip_addr = string
+    mac_addr = string
+    vmid = number
+  }))
+  default = {
+    admin = {
+      ip_addr = "192.168.20.8"
+      mac_addr = "bc:24:11:80:e6:be"
+      vmid = 500
+    }
+    server1 = {
+      ip_addr = "192.168.20.10"
+      mac_addr = "bc:24:11:e6:6a:ec"
+      vmid = 501
+    }
+    server2 = {
+      ip_addr = "192.168.20.11"
+      mac_addr = "bc:24:11:42:16:72"
+      vmid = 502
+    }
+    server3 = {
+      ip_addr = "192.168.20.12"
+      mac_addr = "bc:24:11:e0:60:70"
+      vmid = 503
+    }
+    agent1 = {
+      ip_addr = "192.168.20.13"
+      mac_addr = "bc:24:11:1d:ff:40"
+      vmid = 504
+    }
+    agent2 = {
+      ip_addr = "192.168.20.14"
+      mac_addr = "bc:24:11:bf:39:8a"
+      vmid = 505
+    }
+  }
+}
+
+variable "storage" {
+  type = map(object({
+    ip_addr = string
+    mac_addr = string
+    vmid = number
+  }))
+  default = {
+    storage1 = {
+      ip_addr = "192.168.20.19"
+      mac_addr = "bc:24:11:0b:25:f1"
+      vmid = 510
+    }
+    storage2 = {
+      ip_addr = "192.168.20.20"
+      mac_addr = "bc:24:11:54:0e:43"
+      vmid = 511
+    }
+    storage1 = {
+      ip_addr = "192.168.20.21"
+      mac_addr = "bc:24:11:e4:68:f1"
+      vmid = 512
+    }
+  }
+}
+
+variable "node_token" {
+  type = string
+  default = "K1023b72a50743ad645186e91cf190adddccc0a925103eb0e66a36bc2051df9902e::server:077c602842e9c1107fc74a694dd0d0c0"
 }
